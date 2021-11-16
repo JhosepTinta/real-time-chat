76301c36c7517a1a5b4c48c8b2c2b28a2f282484
jhosep tinta <jtinta86@gmail.com>
2021-11-16 03:39:27 -0400

    da igual

--- src/server.js
+++ src/server.js
@@ -1,23 +1,4 @@
 
-var express = 
-var app = express();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
-app.use(cors());
-
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header('Access-Control-Allow-Methods','DELETE,PUT');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
-app.use(express.static())
 
 const express = require('express');
 const path = require('path');
