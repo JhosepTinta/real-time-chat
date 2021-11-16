cc43d090b7eca0553ece91bda39c2ced60fbc3d5
jhosep tinta <jtinta86@gmail.com>
2021-11-16 03:46:12 -0400

    talvez

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
+var express = require('express'),
+    app = express();
 
+app.use(express.static('src'));
 
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname+'/app/folder'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/app/folder/app.component.html'));
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
-app.listen(process.env.PORT || 8080);
+// API Routes
+// app.get('/blah', routeHandler);
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
